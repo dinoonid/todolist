4e1df66da0554f0e809eca15a3bb591dc8a51950
Dino <dinoonid@dinoonid.com>
2018-02-15 23:22:42 +0100

    Ajout des components TodoInfos, TodoForm et TodoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
+import TodoForm from './TodoForm';
+import TodoInfos from './TodoInfos';
+import TodoList from './TodoList';
 
 class App extends Component {
-afficheOk = () => {alert('ok');}
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+    this.changeTasks = this.changeTasks.bind(this);
+  }
+
+  changeTasks(newTasks) {
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        newTasks
+      ]
+    });
+    console.log(this.state.tasks);
+  }
 
   render() {
     return (<div className="App">
-      <form onSubmit={this.afficheOk}>
-      <input placeholder='Saisissez une tâche'/>
-      <button>Valider</button>
-    </form>
+      <TodoInfos infosCount={this.state.tasks.length}/>
+      <TodoForm tasks={this.state.tasks} changeTasks={this.changeTasks}/>
+      <TodoList list={this.state.tasks}/>
     </div>);
   }
 }
