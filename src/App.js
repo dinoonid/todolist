be5eaa14c3fac2b704d475845b913c594cb87ccd
Dino <dinoonid@dinoonid.com>
2018-02-15 20:40:03 +0100

    Ajout de Sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+afficheOk = () => {alert('ok');}
+
   render() {
     return (<div className="App">
-      <h1>Todolist</h1>
+      <form onSubmit={this.afficheOk}>
+      <input placeholder='Saisissez une tâche'/>
+      <button>Valider</button>
+    </form>
     </div>);
   }
 }
